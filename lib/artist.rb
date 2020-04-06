30d3189634839fd08fd57e2de9ddddbc543430e6
CiaraMaria Proia <proiacm@gmail.com>
2020-04-06 19:54:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,9 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
   
-  def add_song_by_name(song)
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
     
   end
   
